5e7c24a0a401fb197e502074d9f3901255228257
ayushvyas-dev <ayushvyas909@gmail.com>
2026-08-15 00:40:11 +0530

    refactor: simplify pino log level handling

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -4,15 +4,9 @@ import logger from "../config/logger.config.js";
 const loggerMiddleware = pinoHttp({
   logger,
 
-  customLogLevel: (req, res, err) => {
-    if (err || res.statusCode >= 500) {
-      return "error";
-    }
-
-    if (res.statusCode >= 400) {
-      return "warn";
-    }
-
+  customLogLevel: (req, res) => {
+    if (res.statusCode >= 500) return "error";
+    if (res.statusCode >= 400) return "warn";
     return "info";
   },
 
