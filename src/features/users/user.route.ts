f2f070daa7f1177167fc13aa1471ecbde5768d71
ayushvyas-dev <ayushvyas909@gmail.com>
2026-08-20 23:37:51 +0530

    feat(user): add validation

--- src/features/users/user.route.ts
+++ src/features/users/user.route.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import { authenticate } from "../../middlewares/authentication.middleware.js";
 import { deleteMe, getMe, updateMe } from "./user.controller.js";
+import { validate } from "../../middlewares/validate.middleware.js";
+import { updateUserSchema } from "./user.validation.js";
 
 const userRouter = Router();
 
 userRouter.get("/me", authenticate, getMe);
 
-userRouter.patch("/me", authenticate, updateMe);
+userRouter.patch(
+  "/me",
+  authenticate,
+  validate({ body: updateUserSchema }),
+  updateMe,
+);
 
 userRouter.delete("/me", authenticate, deleteMe);
 
